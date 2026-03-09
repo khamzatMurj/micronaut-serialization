75f92ad07eccd94102d89ff0c87be45bc9808bcb
Mousrij Hamza <hmousrij1@gmail.com>
2026-03-09 12:13:09 +0000

    XML

--- serde-xml/src/main/java/io/micronaut/serde/xml/XmlGeneratorEncoder.java
+++ serde-xml/src/main/java/io/micronaut/serde/xml/XmlGeneratorEncoder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.serde.xml;
 
 import io.micronaut.core.type.Argument;
@@ -61,10 +76,13 @@ public final class XmlGeneratorEncoder extends LimitingStream implements Encoder
     public @NonNull Encoder encodeArray(@NonNull Argument<?> type) throws IOException {
         ArrayContext parentCtx = this.arrayContext.peek();
 
-        // Resolve logical XML name: generic placeholder like "E",
+        // Resolve logical XML name.
+        // resolved generic arguments with synthetic names (e.g. "list12") don't leak into the
         String logicalName;
         if (parentCtx != null && isGenericPlaceholder(type.getName())) {
             logicalName = parentCtx.itemName().getLocalPart();
+        } else if (this.currentKey != null) {
+            logicalName = this.currentKey;
         } else {
             logicalName = type.getName(); // type : "List<List<SomeObject E> E> vals" ==> vals
         }
